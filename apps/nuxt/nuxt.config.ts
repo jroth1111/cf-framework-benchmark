d993522d5ecfea80ef6206a3eaeb15bb98a48dcf
gwizz <gwizz@email.com>
2026-03-05 19:20:09 +1100

    disable nuxt devtools in benchmark production config

--- apps/nuxt/nuxt.config.ts
+++ apps/nuxt/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+  compatibilityDate: '2026-03-05',
+  devtools: { enabled: false },
 
   nitro: {
     preset: "cloudflare_module",

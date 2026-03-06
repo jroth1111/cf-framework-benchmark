31120316218f2e9a218946d69d0c1aa0920c67ce
gwizz <gwizz@email.com>
2026-03-07 01:56:21 +1100

    Remove tanstack duplicate smoke markers

--- apps/tanstack-start-solid/src/routes/blog.$slug.tsx
+++ apps/tanstack-start-solid/src/routes/blog.$slug.tsx
@@ -1,7 +1,9 @@
 import { createFileRoute } from '@tanstack/solid-router'
 import { BlogPost } from '../../../solid/src/pages/BlogPost'
+import { benchHeaders } from '../lib/headers'
 
 export const Route = createFileRoute('/blog/$slug')({
+  headers: () => benchHeaders('public, max-age=0, s-maxage=300'),
   component: BlogPostPage,
 })
 
--- apps/tanstack-start-solid/src/routes/stays.$id.tsx
+++ apps/tanstack-start-solid/src/routes/stays.$id.tsx
@@ -1,7 +1,9 @@
 import { createFileRoute } from '@tanstack/solid-router'
 import { StayDetail } from '../../../solid/src/pages/StayDetail'
+import { benchHeaders } from '../lib/headers'
 
 export const Route = createFileRoute('/stays/$id')({
+  headers: () => benchHeaders('public, max-age=0, s-maxage=300'),
   component: StayDetailPage,
 })
 

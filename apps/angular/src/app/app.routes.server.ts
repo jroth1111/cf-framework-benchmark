deab2fa33384cc9d7d0a50a00d4526d03f4a41ec
gwizz <gwizz@email.com>
2026-03-06 17:18:31 +1100

    Prerender static shells in Astro and Angular

--- apps/angular/src/app/app.routes.server.ts
+++ apps/angular/src/app/app.routes.server.ts
@@ -2,6 +2,26 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'stays',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'blog',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'chart',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'media',
+    renderMode: RenderMode.Prerender,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Server,
   },
